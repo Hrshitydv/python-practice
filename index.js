eac73698ec6b4718d043cc0100bbc1c38a648055
Harshit <hydv9958@gmail.com>
2026-05-12 11:20:49 +0530

    test

---
+++ index.js
@@ -0,0 +1,19 @@
+const age =19
+const name="Harshit"
+const DOB= "23-08-06"
+
+const age=20
+const name="Siddharth"
+const DOB="18-01-07"
+
+const age =21
+const name ="shivam"
+const DOB= "18-11-06"
+
+const age= 22
+const name="Himanshu"
+const DOB="18-08-05"
+
+const age=23
+const name="Lakshay"
+const DOB="20-09-03"
